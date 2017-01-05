20b3f9ab117f5e313914d3482e067a2c898dd457
Jessy <jessy.bellanger.76@gmail.com>
2017-01-05 09:33:16 +0100

    Test pour voir si j'arrive à commit

---
+++ src/Vue/CUI/CUI.java
@@ -0,0 +1,13 @@
+package Vue.CUI;
+
+import Vue.IVue;
+
+/**
+ * Classe qui gère l'affichage en mode console (Console User Interface)
+ *
+ * @author Win'Rs
+ * @version 20170105
+ */
+public class CUI implements IVue {
+
+}
---
+++ src/Vue/IVue.java
@@ -0,0 +1,11 @@
+package Vue;
+
+/**
+ * Interface qui gère les deux modes de vue (CUI et GUI)
+ *
+ * @author Win'Rs
+ * @version 20170105
+ */
+public interface IVue {
+
+}
