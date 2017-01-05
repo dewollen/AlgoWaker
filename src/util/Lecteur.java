2523f15a60e9d22433669635456f2831f02d1a84
Jessy <jessy.bellanger.76@gmail.com>
2017-01-05 11:04:15 +0100

    Ajout de la classe Lecteur

--- src/util/Lecteur.java
+++ src/util/Lecteur.java
@@ -1,9 +1,33 @@
 package util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
- * Created by Jessy on 05/01/2017.
+ * Classe qui sert à lire un fichier d'entrée .algo
+ *
+ * @author Win'Rs
+ * @version 2017-01-05
  */
 public class Lecteur {
-    private String fichier;
+    private HashMap<Integer, String> numLignes;
 
+    public Lecteur(String fichier) {
+        this.numLignes = new HashMap<Integer, String>();
+
+        try {
+            Scanner scFichier = new Scanner(new File("fichier"));
+
+            int compteur = 0;
+            while(scFichier.hasNextLine()) {
+                this.numLignes.put(compteur, scFichier.nextLine());
+
+                compteur++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Le fichier spécifié est introuvable.");
+        }
+    }
 }
