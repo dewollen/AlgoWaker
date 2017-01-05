2372b4320d8684dc0c9ed600b50af1a0bf657ef5
Jessy <jessy.bellanger.76@gmail.com>
2017-01-05 10:16:12 +0100

    Bonjour monde !

---
+++ src/metier/Traducteur.java
@@ -0,0 +1,10 @@
+package metier;
+
+/**
+ * Classe qui interprète les portions de pseudo-code afin d'agir en conséquence
+ *
+ * @author Win'Rs
+ * @version 2017-01-05
+ */
+public class Traducteur {
+}
