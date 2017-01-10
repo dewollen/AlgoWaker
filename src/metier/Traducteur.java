d8462e94bacb08c02bb96b9ed88e50a90a3c8507
thomasdigregorio <thomasdigregorio@me.com>
2017-01-10 15:45:43 +0100

    Correction Traducteur

--- src/metier/Traducteur.java
+++ src/metier/Traducteur.java
@@ -17,25 +17,24 @@ import java.util.Scanner;
 public class Traducteur {
     private Interpreter interpreter;
 
-    // $$$$$$$$$$$$$$$$$$$$$$$$$
-    // $$$$$$$$$$$$$$$$$$$$$$$$$
-    // $$$$$$$$$$$$$$$$$$$$$$$$$
-    //
-    // Effectuer les changements liés à la double arraylist
-    // Découper traiterLigne dans initConstante et initVariable
-    //
-    // $$$$$$$$$$$$$$$$$$$$$$$$$
-    // $$$$$$$$$$$$$$$$$$$$$$$$$
-    // $$$$$$$$$$$$$$$$$$$$$$$$$
     private IVue vue;
+
+    private boolean creerCons;
+    private boolean creerVar;
+    private boolean debutAlgo;
 
     private ArrayList<Donnee> alConstante;
     private ArrayList<ArrayList<Donnee>> alEtatVariable;
-    private ArrayList<String>  alConsole;
+
+    private ArrayList<String> alConsole;
 
     public Traducteur(IVue vue) {
         this.vue = vue;
         this.interpreter = new Interpreter();
+
+        this.creerCons = false;
+        this.creerVar = false;
+        this.debutAlgo = false;
 
         this.alConstante = new ArrayList<>();
         this.alEtatVariable = new ArrayList<>();
@@ -43,108 +42,142 @@ public class Traducteur {
         this.alConsole = new ArrayList<>();
     }
 
-    /*public void initConstante(String ligne) {
-
-    }
-
-    public void initVariable(String ligne) {
-
-    }*/
-
-    public void initAttribut(String ligne, boolean creerCons) {
-        if(!ligne.equals("")) {
+    public void initAttribut(String ligne, boolean creerCons, ArrayList<Donnee> alVarTemp) {
+        if (!ligne.equals("")) {
             String[] tabLigne;
             String type;
 
-            tabLigne = ligne.split("<-");
+            if (creerCons)
+                tabLigne = ligne.split("<-");
+            else
+                tabLigne = ligne.split(":");
             tabLigne[0] = tabLigne[0].replaceAll("[\t ]", "");
             tabLigne[1] = tabLigne[1].replaceAll(" ", "");
             String[] tabNomAttribut = tabLigne[0].split(",");
 
             if (creerCons) {
                 type = rechercheType(tabLigne[1]);
-                ajouteConstante(tabNomAttribut, type, tabLigne[1]);
+                ajouterConstante(tabNomAttribut, type, tabLigne[1]);
             } else {
                 type = tabLigne[1];
-                this.alEtatVariable.add(ajouteVariable(tabNomAttribut, type));
+                ArrayList<Donnee> alVariable = ajouterVariable(tabNomAttribut, type);
+                for (int i = 0; i < alVariable.size(); i++) {
+                    alVarTemp.add(alVariable.get(i));
+                }
+                this.alEtatVariable.add(alVarTemp);
             }
         }
     }
 
     public void traiterLigne(String ligne, int numLigne) {
-
-        // Affectation !!!
-        if (ligne.contains("<-")) {
-            String[] tabAffectation = ligne.split("<-");
-            tabAffectation[0] = tabAffectation[0].replaceAll("[\t ]", "");
-            tabAffectation[1] = tabAffectation[1].replaceAll(" ", "");
-            System.out.println(tabAffectation[0] + " --> " + tabAffectation[1]);
-            try {
-                ArrayList<Donnee> alVariable = alEtatVariable.get(numLigne);
-                interpreter.eval("" + tabAffectation[0] + " = " + tabAffectation[1]);
-                System.out.println(interpreter.get(tabAffectation[0]));
-                for (int i = 0; i < alVariable.size(); i++) {
-                    if (alVariable.get(i).getNom().equals(tabAffectation[0])) {
-                        alVariable.get(i).setValeur("" + interpreter.get(tabAffectation[0]));
-                    }
-                }
-            } catch (EvalError evalError) {
-                evalError.printStackTrace();
-            }
+        ligne = ligne.replaceAll("◄—", "<-");
+        ArrayList<Donnee> alVarTemp;
+        if (numLigne != 0) {
+            alVarTemp = (ArrayList<Donnee>) this.getAlEtatVariable().get(numLigne - 1).clone();
+        } else {
+            alVarTemp = new ArrayList<>();
         }
+        if (ligne.toLowerCase().contains("constante")) {
+            alEtatVariable.add(alVarTemp);
+            this.creerCons = true;
+        } else if (ligne.toLowerCase().contains("variable")) {
+            alEtatVariable.add(alVarTemp);
+            this.creerCons = false;
+            this.creerVar = true;
+        } else if (ligne.toLowerCase().contains("debut")) {
+            alEtatVariable.add(alVarTemp);
+            this.creerVar = false;
+            this.debutAlgo = true;
+        } else if (creerCons || creerVar) {
+            if (!ligne.equals("")) {
+                if (creerCons) this.alEtatVariable.add(alVarTemp);
+                initAttribut(ligne, creerCons, alVarTemp);
+            } else {
+                this.alEtatVariable.add(alVarTemp);
+            }
+        } else if (debutAlgo) {
 
-        // ECRIRE !!!
-        if (ligne.contains("\tecrire ")) {
-            String[] tabEcrire = ligne.split("[()]");
-            tabEcrire[1] = tabEcrire[1].replaceAll(" ", "");
-            String[] tabSOP = tabEcrire[1].split("&");
+            this.alEtatVariable.add(alVarTemp);
 
-            try {
-                String sTemp = "";
-                for (int i = 0; i < tabSOP.length; i++) {
-                    if (tabSOP[i].contains("\"")) {
-                        sTemp += tabSOP[i].replaceAll("\"", "");
-                    } else {
-                        sTemp += interpreter.get(tabSOP[i]);
+            // Affectation !!!
+            if (ligne.contains("<-")) {
+                System.out.println("ZBEEEEEEEUB");
+                String[] tabAffectation = ligne.split("<-");
+                tabAffectation[0] = tabAffectation[0].replaceAll("[\t ]", "");
+                tabAffectation[1] = tabAffectation[1].replaceAll(" ", "");
+                try {
+                    ArrayList<Donnee> alVariable = alEtatVariable.get(numLigne);
+                    interpreter.eval("" + tabAffectation[0] + " = " + tabAffectation[1]);
+                    System.out.println(interpreter.get(tabAffectation[0]));
+                    for (int i = 0; i < alVariable.size(); i++) {
+                        if (alVariable.get(i).getNom().equals(tabAffectation[0])) {
+                            alVariable.get(i).setValeur("" + interpreter.get(tabAffectation[0]));
+                        }
                     }
+                } catch (EvalError evalError) {
+                    evalError.printStackTrace();
                 }
-                alConsole.add(sTemp);
-            } catch (EvalError evalError) {
-                evalError.printStackTrace();
             }
-        }
 
-        // LIRE !!!
-        if(ligne.contains("\tlire ")){
-            String valeur = vue.lire();
-            String variable = ligne.substring(ligne.indexOf("(")+1,ligne.indexOf("("));
-            try {
-                ArrayList<Donnee> alVariable = alEtatVariable.get(numLigne);
-                interpreter.eval("" + variable + "=" + valeur);
-                for (int i = 0; i < alEtatVariable.size(); i++) {
-                    if (alVariable.get(i).getNom().equals(variable))
-                        alVariable.get(i).setValeur("" + interpreter.get(variable));
+            // ECRIRE !!!
+            if (ligne.contains("\tecrire ")) {
+                String[] tabEcrire = ligne.split("[()]");
+                tabEcrire[1] = tabEcrire[1].replaceAll(" ", "");
+                String[] tabSOP = tabEcrire[1].split("&");
+
+                try {
+                    String sTemp = "";
+                    for (int i = 0; i < tabSOP.length; i++) {
+                        if (tabSOP[i].contains("\"")) {
+                            sTemp += tabSOP[i].replaceAll("\"", "");
+                        } else {
+                            sTemp += interpreter.get(tabSOP[i]);
+                        }
+                    }
+                    alConsole.add(sTemp);
+                } catch (EvalError evalError) {
+                    evalError.printStackTrace();
                 }
             }
-            catch (EvalError evalError) {
-                evalError.printStackTrace();
+
+            // LIRE !!!
+            if (ligne.contains("\tlire ")) {
+                String valeur = vue.lire();
+                String variable = ligne.substring(ligne.indexOf("(") + 1, ligne.indexOf(")"));
+                try {
+                    ArrayList<Donnee> alVariable = alEtatVariable.get(numLigne);
+                    interpreter.eval("" + variable + "=" + valeur);
+                    for (int i = 0; i < alEtatVariable.size(); i++) {
+                        if (alVariable.get(i).getNom().equals(variable))
+                            alVariable.get(i).setValeur("" + interpreter.get(variable));
+                    }
+                } catch (EvalError evalError) {
+                    evalError.printStackTrace();
+                }
             }
+        } else {
+            alEtatVariable.add(alVarTemp);
         }
     }
 
     private String rechercheType(String s) {
-        if      (s.contains("\"")   ) { return "Chaine de caractere"; }
-        else if (s.contains("\'")   ) { return "Caractere";           }
-        else if (s.contains(",")    ) { return "Reel";                }
-        else if (s.matches("[0-9]+")) { return "Entier";              }
-        else if (s.toLowerCase().equals("vrai") || s.toLowerCase().equals("faux")) { return "booleen"; }
-        else {
+        if (s.contains("\"")) {
+            return "Chaine de caractere";
+        } else if (s.contains("\'")) {
+            return "Caractere";
+        } else if (s.contains(",")) {
+            return "Reel";
+        } else if (s.matches("[0-9]+")) {
+            return "Entier";
+        } else if (s.toLowerCase().equals("vrai") || s.toLowerCase().equals("faux")) {
+            return "booleen";
+        } else {
             System.out.println("Variable non trouvable");
             return "";
         }
     }
 
-    private void ajouteConstante(String[] tab, String type, String valeur) {
+    private void ajouterConstante(String[] tab, String type, String valeur) {
         type = type.toLowerCase();
         String typeTemp = "";
         boolean suivi = false;
@@ -180,7 +213,7 @@ public class Traducteur {
                     break;
             }
 
-            if(!typeTemp.equals("error")) {
+            if (!typeTemp.equals("error")) {
                 try {
                     interpreter.eval(typeTemp + tab[i].toUpperCase() + " = " + valeur);
                 } catch (EvalError evalError) {
@@ -190,7 +223,7 @@ public class Traducteur {
         }
     }
 
-    public ArrayList<Donnee> ajouteVariable(String[] tab, String type) {
+    public ArrayList<Donnee> ajouterVariable(String[] tab, String type) {
         ArrayList<Donnee> alVariable = new ArrayList<Donnee>();
         type = type.toLowerCase();
         String s = "";
@@ -243,10 +276,12 @@ public class Traducteur {
                     break;
             }
 
-            if(!typeTemp.equals("error")) {
+            if (!typeTemp.equals("error")) {
                 try {
                     interpreter.eval(typeTemp + tab[i]);
-                }catch (EvalError evalError) { evalError.printStackTrace(); }
+                } catch (EvalError evalError) {
+                    evalError.printStackTrace();
+                }
             }
         }
 
