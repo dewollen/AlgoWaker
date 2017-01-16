78693007525e7459d0e7347309cf59294b6d2615
Dawul <leuret.wulfran@hotmail.fr>
2017-01-16 09:35:09 +0100

    Ajout regex Variables et constante

--- src/metier/utile/Regex.java
+++ src/metier/utile/Regex.java
@@ -7,13 +7,15 @@ package metier.utile;
  * @version 15/01/2017
  */
 public abstract class Regex {
-    public static final String LIRE    = "^lire[(].+[)]\\\\*.*";
-    public static final String ECRIRE  = "^ecrire[(].+[)]\\\\*.*";
-    public static final String SI      = "si.*alors";
-    public static final String SINON   = "sinon";
-    public static final String FSI     = "fsi";
-    public static final String TANTQUE = "tantque.*faire";
-    public static final String FTQ     = "ftq";
+    public static final String LIRE      = "^lire[(].+[)]\\\\*.*";
+    public static final String ECRIRE    = "^ecrire[(].+[)]\\\\*.*";
+    public static final String SI        = "si.*alors";
+    public static final String SINON     = "sinon";
+    public static final String FSI       = "fsi";
+    public static final String TANTQUE   = "tantque.*faire";
+    public static final String FTQ       = "ftq";
+    public static final String VARIABLE  = "^[a-z].*:[A-Za-z]+";
+    public static final String CONSTANTE = "[A-Z]+<-.+";
 
     public static boolean correspond(String expression, String regex) {
         return expression.replaceAll(" ", "").replaceAll("\t", "").toLowerCase().matches(regex);
